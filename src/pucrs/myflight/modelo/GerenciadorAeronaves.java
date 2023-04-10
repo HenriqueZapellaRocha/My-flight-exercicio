ea96453225983f8bec0ae07e3f184ef09c3b1034
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-04-10 18:55:30 -0300

    Clase GerenciadorAeronaves implementada

--- src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,5 +1,32 @@
 package pucrs.myflight.modelo;
+import java.util.ArrayList;
 
 public class GerenciadorAeronaves {
+
+    private ArrayList<Aeronave> aeronaves;
+
+    public GerenciadorAeronaves(ArrayList<Aeronave> aeronaves) {
+        aeronaves  = new ArrayList<Aeronave>();
+    }
+
+    public void adicionar(Aeronave aviao) {
+        aeronaves.add(new Aeronave(aviao.getCodigo(), aviao.getDescricao(), aviao.getCapacidade())); 
+    }
+
+    public ArrayList<Aeronave> listarTodas() {
+        return aeronaves;
+    }
+
+    public Aeronave buscarCodigo(String cod) {
+
+        for(Aeronave aviao : aeronaves) {
+            if(aviao.getCodigo() == cod) {
+                return aviao;
+            }
+        }
+        return null;
+    }
+
+   
 
 }
