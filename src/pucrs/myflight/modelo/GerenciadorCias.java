5b5e11ffc176746c3291d25844851cc9834fbb46
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-04-10 18:45:13 -0300

    classe GerenciadorDeCias implementada

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -1,11 +1,40 @@
 package pucrs.myflight.modelo;
-
 import java.util.ArrayList;
 
 public class GerenciadorCias {
 	private ArrayList<CiaAerea> empresas;
 	
 	public GerenciadorCias() {
-		empresas = new ArrayList<>();
+		empresas = new ArrayList<CiaAerea>();
 	}
+
+	public void adicionar(CiaAerea cia) {
+	empresas.add(new CiaAerea(cia.getCodigo(), cia.getNome()));
+	}
+
+	public ArrayList<CiaAerea> listarTodas() {
+		return empresas;
+	}
+
+	public CiaAerea buscarCodigo(String cod) {
+
+		for(CiaAerea cia: empresas) {
+			if (cia.getCodigo() == cod) {
+				return cia;
+		}
+		}
+
+		return null;
+		}
+
+	public CiaAerea buscarNome(String cod) {
+
+		for(CiaAerea cia: empresas) {
+			if (cia.getNome() == cod) {
+				return cia;
+			}
+		}
+		
+		return null;
+		}
 }
