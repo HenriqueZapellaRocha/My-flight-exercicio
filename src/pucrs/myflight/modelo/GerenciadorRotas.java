091fa3db93f7fbc7ed5b5649c6ab409f94aaa30f
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-04-11 12:23:37 -0300

    GerenciadorRotas adicionado

--- src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,5 +1,32 @@
 package pucrs.myflight.modelo;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class GerenciadorRotas {
 
+	private ArrayList<Rota> rotas;
+
+	public GerenciadorRotas(ArrayList<Rota> rotas) {
+        rotas = new ArrayList<Rota>();
+    }    
+
+    public void adicionar(Rota rota) {
+        rotas.add(rota);
+    }
+
+    public ArrayList<Rota> listarTodas() {
+        return rotas;
+    }
+
+    public ArrayList<Rota> buscarPorOrigem(Aeroporto origem) {
+        ArrayList<Rota> rotasEncotradas = new ArrayList<Rota>();
+
+        for(Rota buscar : rotas) {
+            if(buscar.getOrigem() == origem) {
+                rotasEncotradas.add(buscar);
+            }
+        }
+        return rotasEncotradas;
+    }
 }
